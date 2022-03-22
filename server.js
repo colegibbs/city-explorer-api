baed0fb1f4f5929fd497cc16043bfbc86d184e52
Cole Gibbs <colegibbs0@gmail.com>
2022-03-22 16:32:33 -0700

    set up require and use

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 'use strict';
+console.log('My first server');
 
 //REQUIRE
+const express = require('express');
+require('dotenv').config();
+let data = require('./data/weather.json');
+const cors = require('cors');
 
 //USE
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3002;
 
 //ROUTES
 
